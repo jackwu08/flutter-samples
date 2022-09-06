52f31d407a6a4a8f5ef013c77fffa4f72c4372c4
jack wu <104415289+jackwu08@users.noreply.github.com>
2022-09-06 11:50:37 +0800

    18

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -10,4 +10,13 @@ void main() {
 
     expect(counter.value, 1);
   });
+   test('String.split() splits the string on the delimiter', () {
+    var string = 'foo,bar,baz';
+    expect(string.split(','), equals(['foo', 'bar', 'baz']));
+  });
+
+  test('String.trim() removes surrounding whitespace', () {
+    var string = '  foo ';
+    expect(string.trim(), equals('foo'));
+  });
 }
