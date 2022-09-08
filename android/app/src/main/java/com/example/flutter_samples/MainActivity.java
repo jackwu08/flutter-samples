7d3ad243b3dc9aa21b7e7c16f31fb435692f0238
jack wu <104415289+jackwu08@users.noreply.github.com>
2022-09-08 12:04:58 +0800

    18

--- android/app/src/main/java/com/example/flutter_samples/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_samples/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.flutter_samples;
 
-//import android.os.Bundle;
+import android.os.Bundle;
 //import io.flutter.app.FlutterActivity;
 //import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.embedding.android.FlutterActivity;
