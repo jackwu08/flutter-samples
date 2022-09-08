be61db37f439e0c499f580d6901d1b5d17896921
jack wu <104415289+jackwu08@users.noreply.github.com>
2022-09-08 15:40:34 +0800

    22

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -19,7 +19,7 @@ class HomePage extends StatelessWidget {
         title: const Text('Testing Sample'),
         actions: [
           TextButton.icon(
-            style: TextButton.styleFrom(Color: Colors.white),
+            style: TextButton.styleFrom(primary: Colors.white),
             onPressed: () {
               Navigator.pushNamed(context, FavoritesPage.routeName);
             },
