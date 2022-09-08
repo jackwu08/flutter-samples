aa232eea09cc310c3de6628fe0b1e23e55173576
jack wu <104415289+jackwu08@users.noreply.github.com>
2022-09-08 12:24:16 +0800

    17

--- android/app/src/main/java/com/example/flutter_samples/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_samples/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.flutter_samples;
 
 import android.os.Bundle;
 //import io.flutter.app.FlutterActivity;
-//import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.embedding.android.FlutterActivity;
 
 import android.view.ViewTreeObserver;
